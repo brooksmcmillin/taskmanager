9ab2a0bbe7db94997260ba4cdf42e1fb528135e3
brooksmcmillin <brooks@brooksmcmillin.com>
2026-01-04 22:08:12 -0800

    Fix bcrypt issue (#83)

--- src/lib/db.js
+++ src/lib/db.js
@@ -2,7 +2,7 @@ import pg from 'pg';
 const { Pool } = pg;
 import { config } from 'dotenv';
 import crypto from 'crypto';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 
 config();
 
